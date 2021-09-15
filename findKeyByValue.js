162cabd97514764da255a38c88e3ec4e16f16709
Chitra Venkitaraman <chitravenkitraman@gmail.com>
2021-09-15 19:46:27 +0000

    added findByvalue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+// AssertEqual function
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`Assertion Passed ${actual} === ${expected}`);
+    } else  console.log(`Assertion Failed  ${actual} !==  ${expected}`);
+
+  };
+ // Find key by value function
+ const findKeyByValue = function(object,value) {
+     for (const property in object) {
+       if(object[property] === value)
+       return(property);
+     }
+ };
+
+
+ // test
+ const bestTVShowsByGenre = {
+    sci_fi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire"
+  };
+
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
