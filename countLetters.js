3e97d63c82b379923f1aee83850701871ed79485
Chitra Venkitaraman <chitravenkitraman@gmail.com>
2021-09-15 19:12:19 +0000

    Added countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,19 @@
+// Assert Equalfunction
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`Assertion Passed ${actual} === ${expected}`);
+    } else  console.log(`Assertion Failed  ${actual} !==  ${expected}`);
+
+  };
+ // Count letter function
+ const countLetters = function(string) {
+ const results = {};
+ string = string.replace(/ /g,'');
+ for (const letter of string) {
+     if(results[letter]) results[letter] += 1;
+     else results[letter] = 1;
+ }
+ return results
+ };
+ // test
+ console.log(countLetters("lighthouse in the house"));
