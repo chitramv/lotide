ee0fa5dda57a04ac812b311acef8103450f126df
Chitra <chitravenkitraman@gmail.com>
2021-09-20 14:33:03 -0600

    modified test for tail.js using mocha-chai

--- tail.js
+++ tail.js
@@ -1,4 +1,4 @@
-const assertEqual = require('./assertEqual');
+// Function to return tail of an array (tail = array - head)
 const tail = function(array) {
   let arraynew = array.slice(1);
   return (arraynew);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,23 @@
 // TEST CODE
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
-const numbers = [5,3,4,6,8];
-const tailnum = tail(numbers);
-for (let i = 0; i < numbers.length; i++) {
-  assertEqual(numbers[i + 1], tailnum[i]);
-}
+//const assertEqual = require('../assertEqual');
+//const tail = require('../tail');
+//const numbers = [5,3,4,6,8];
+//const tailnum = tail(numbers);
+//for (let i = 0; i < numbers.length; i++) {
+  //assertEqual(numbers[i + 1], tailnum[i]);
+//}
+
+// Tail test using mocha and chai
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("returns [4,3,8] for [5,4,3,8]", () => {
+    assert.deepEqual(tail([5,4,3,8]), [4,3,8]);
+  });
+
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(tail(['5']), []);
+  });
+
+});
